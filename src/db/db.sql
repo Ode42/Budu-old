9b854749bcc346b90dcdc948446ca353ae0084f7
Ode42 <otso.kurkela@outlook.com>
2021-08-07 21:28:59 +0300

    Added SQL statements for initializing database

--- src/db/db.sql
+++ src/db/db.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE budu;
+CREATE TABLE budjets(
+    budjetId SERIAL PRIMARY KEY,
+    budjetName VARCHAR(255),
+    initialAmount SERIAL,
+    costCentres TEXT[]
+);
+
+INSERT INTO budjets(budjetid, budjetname, initialamount, costcentres)
+VALUES (0, "Testibudjetti", 100, '{"Ruoka", "Opiskelu"}');
