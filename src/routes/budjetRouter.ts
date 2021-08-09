cee2eb24f882701f4fbf5ad8c99aa5d9dbf747f8
Ode42 <otso.kurkela@outlook.com>
2021-08-09 10:59:44 +0300

    Budjet receiving endpoint

--- src/routes/budjetRouter.ts
+++ src/routes/budjetRouter.ts
@@ -14,16 +14,28 @@ budjetRouter.get("/", async (request:Request, response:Response) => {
         console.error(error);
     };
 })
-budjetRouter.get("/:id", (request:Request, response:Response) => {
-    const budjets:Budjet[] = [{
-        budjetId: 0,
-        budjetName: "Test budjet",
-        initialAmount: 1700,
-        costCentres: ["IT", "Studying"]
-    }]
-    const budjet = budjets.find((budjet) => budjet.budjetId.toString() === request.params.id)
-    response.send(budjet);
+
+budjetRouter.get("/:budjet_id", async (request:Request, response:Response) => {
+    try {
+        const {budjet_id} = request.params;
+        const budjet = await pool.query("SELECT * FROM budjets WHERE budjet_id = $1", [budjet_id]);
+        response.json(budjet.rows[0]);
+    } catch (error) {
+        console.error(error);
+        response.status(401);
+    }
 })
+
+budjetRouter.post("/", async (request:Request, response:Response) => {
+    try {
+        const {budjet_name} = request.body;
+        const {budjet_balance} = request.body;
+        const newBudjet = await pool.query("INSERT INTO budjets (budjet_name, budjet_balance) VALUES ($1, $2) RETURNING *", [budjet_name, budjet_balance]);
+        response.json(newBudjet.rows[0]);
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 
 export default budjetRouter;
