572d1080f9b0ba3d0d2df501452583bb22a6909e
Ode42 <otso.kurkela@outlook.com>
2021-08-10 13:02:29 +0300

    Delete unused SQL table

--- src/db/db.sql
+++ src/db/db.sql
@@ -4,8 +4,3 @@ CREATE TABLE budjets(
     budjet_name VARCHAR(50) UNIQUE NOT NULL,
     budjet_balance SERIAL NOT NULL
 );
-CREATE TABLE cost_centres(
-    budjet_id SERIAL NOT NULL,
-    cost_centre VARCHAR(50) NOT NULL,
-    cost_centre_balance SERIAL NOT NULL
-);
